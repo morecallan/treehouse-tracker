e9174877fa0cb4de6d681c929152ea350991ff58
Callan Morrison <callan.l.morrison@gmail.com>
2017-01-18 22:01:48 -0600

    init server boilerplate

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 6060
+app.set('port', port)
+const request = require('request');
+
+// MIDDLEWARE (transform stream | allow CORS)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.get('/:classname', (req, res) => {
+  let apiCall = req.params.classname
+  console.log(apiCall)
+  // let apiReq = `http://api.petfinder.com/${apiCall}`
+  // request.get(apiReq, (err, _, body) => {
+  //   res.send(body)
+  // });
+});
+
+app.listen(port, () =>
+  console.log(`Listening on port: ${port}`)
+)
